818a30e021d8d033abc51840a1693cf1556bda4b
psj98 <park98sj1@hanmail.net>
2021-06-05 23:14:53 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 // import logo from './logo.svg';
 import * as tf from "@tensorflow/tfjs";
 import * as bodyPix from "@tensorflow-models/body-pix";
